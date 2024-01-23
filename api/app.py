eabcafea160fb5c00364e912fdb7503a17dbefca
vaibhavvemani <vaibhavvemani@gmail.com>
2024-01-23 18:12:04 +0530

    Added upstash redis support

--- api/app.py
+++ api/app.py
@@ -2,18 +2,21 @@ from flask import Flask
 from flask import request, redirect, session
 from upstash_redis import Redis
 from markupsafe import escape
+import os
 
 app = Flask(__name__)
 
-redis = Redis(url="https://apn1-emerging-baboon-34212.upstash.io", token="AYWkACQgYjhmZWQxNWQtZjAwYy00OGI1LThiYTYtNjUwYWRkOGY2ZWQyZDc5MGM4MjllMzIxNDkzNTg2YmJjMzUwMTk2MDM0NmU=")
+vercel_url = os.getenv('CACHE_URL')
+vercel_token = os.getenv('CACHE_TOKEN')
+redis = Redis(url=vercel_url, token=vercel_token)
 
 @app.post('/hello')
 def hello():
     username = request.form['username']
-    redis.set("username", f"{escape(username)}")
-    return f'<h1>Hello {escape(username)}!</h1>'
+    message = request.form['msg']
+    redis.set(f"{escape(username)}", f"{escape(message)}")
 
-@app.get('/hello')
-def gethello():
-    username = redis.get("username")
-    return f'<h1>Hello {escape(username)}!</h1>'
+@app.get('/messages/<username>')
+def gethello(username):
+    message = redis.get(f"{escape(username)}")
+    return f'<h1>f{escape(username)} has sent {escape(message)}!</h1>'
