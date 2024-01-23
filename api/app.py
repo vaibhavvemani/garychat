f063a8642e515fad6fdeed00a0578a3aeec57079
vaibhavvemani <vaibhavvemani@gmail.com>
2024-01-23 15:47:05 +0530

    Testing HTML escaping

--- api/app.py
+++ api/app.py
@@ -1,7 +1,11 @@
 from flask import Flask
-
+from markupsafe import escape
 app = Flask(__name__)
 
-@app.get('/hello')
+@app.get('/hello/<username>')
+def hello(username):
+    return f'<h1>Hello {escape(username)}!</h1>'
+
+@app.post('/hello')
 def hello():
     return '<h1>Hello Back!</h1>'
